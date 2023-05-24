f51cf5d310faca39429d218d3e97991f9af974bb
LorisMarzocchi <ml77182a@gmail.com>
2023-05-24 17:19:05 +0200

    createIndex

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  methods: {
+    requestData() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/550?api_key=95f92b62827d48d27c5483a9415a484d`
+        )
+        .then((response) => (this.store.cardList = response.data.data));
+    },
+  }
+
+}
+
+
+
+
+// https://api.themoviedb.org/3/movie/550?api_key=95f92b62827d48d27c5483a9415a484d
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
